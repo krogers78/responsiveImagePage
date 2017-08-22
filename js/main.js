4a0418faddf87d2a9e29afa3c71980c52bab2d67
Brett Rogers <bdrogers@fullsail.edu>
2017-08-22 13:18:18 -0400

    Successfully pull information from the API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+const apiURL = 'https://api.unsplash.com/search/photos/?query=sharks&client_id=3c8ef444b6f9284acd6886216ac7ec6b0f062a88e4b4211e6314278df06276ff'
 const imageSection = document.querySelector('#imagesSection')
+const options = { method: 'GET' }
+let data = []
+
+fetch (apiURL, options)
+  .then(response => response.json())
+  .then(responseAsJson => {
+    data = responseAsJson.results
+    console.log(data)
+
+    // data.forEach((item, index) => {
+    //   console.log(item.urls.small)
+    // })
+    // ImagePopulation(data)
+  })
+    .catch(error => {
+      console.log('An Error Occured:', error)
+    })
+
+function ImagePopulation(arrayData) {
+  arrayData.forEach((item, index) => {
+    imagesSection.insertAdjacentHTML('beforeEnd', `<article>
+                                                      <img src="${item.urls.small}">
+                                                      <div>
+                                                          <img src="./images/user-icon.svg">
+                                                          <p>${item.user.name}</p>
+                                                      </div>
+                                                      <div>
+                                                          <img src="./images/heart.svg">
+                                                          <p>${item.likes}</p>
+                                                      </div>
+                                                    </article>
+                                                      `)
+  })
+}
